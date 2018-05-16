dfabf81536470fe1fc7575294cc3f271b320b73e
MiF <grigorchuk.konstantin@gmail.com>
2018-05-16 22:20:10 +0300

    HW1

--- admin/src/routes/admin.js
+++ admin/src/routes/admin.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import UserForm from '../components/user/user'
+import { addUser } from '../ducks/user'
 
 class AdminPage extends Component {
     static propTypes = {
@@ -9,9 +12,12 @@ class AdminPage extends Component {
         return (
             <div>
                 <h1>Admin Page</h1>
+                <UserForm onSubmit = {this.addUser}/>
             </div>
         )
     }
+
+    addUser = ({ first, last, email }) => this.props.addUser(first, last, email)
 }
 
-export default AdminPage
+export default connect(null, { addUser })(AdminPage)
