3005b5b20a4f053856db6490c5bb36041106245c
Denis <someden@ya.ru>
2018-05-20 16:26:32 +0000

    add events

--- admin/src/ducks/utils.js
+++ admin/src/ducks/utils.js
@@ -1,3 +1,11 @@
+import { OrderedMap } from 'immutable'
+
 export function generateId() {
   return Date.now() + Math.random()
 }
+
+export const objectToOrderedMap = (obj, ItemRecord) =>
+  Object.entries(obj).reduce(
+    (acc, [id, value]) => acc.set(id, ItemRecord({ id, ...value })),
+    OrderedMap()
+  )
