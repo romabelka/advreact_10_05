941df8e2740a8f2563fd401ac4928504e3fa4744
Roman Iakobchuk <romania@wix.com>
2018-05-21 18:07:48 +0300

    add HW2

--- admin/src/ducks/utils.js
+++ admin/src/ducks/utils.js
@@ -1,3 +1,12 @@
+import { List } from 'immutable'
+
 export function generateId() {
   return Date.now() + Math.random()
+}
+
+export function fbToEntities(values, DataRecord) {
+  return Object.entries(values).reduce(
+    (acc, [uid, value]) => acc.push(new DataRecord({ uid, ...value })),
+    new List([])
+  )
 }
