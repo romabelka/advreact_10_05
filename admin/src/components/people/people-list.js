c9211dc4887dd0f922863b23bc71525e26eceb70
omiceron <omiceron@gmail.com>
2018-06-06 16:03:18 +0500

    HT5: Simple and partly working TransitMotion in people-list

--- admin/src/components/people/people-list.js
+++ admin/src/components/people/people-list.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { peopleSelector, fetchAllPeople } from '../../ducks/people'
 import { List } from 'react-virtualized'
 import PersonCard from './person-card'
+import { TransitionMotion, spring } from 'react-motion'
 
 import 'react-virtualized/styles.css'
 
@@ -26,10 +27,40 @@ class PeopleList extends Component {
   rowRenderer = ({ style, index, key }) => {
     const person = this.props.people[index]
     return (
-      <div style={style} key={key}>
-        <PersonCard person={person} />
-      </div>
+      <TransitionMotion
+        styles={this.getStyles()}
+        willEnter={this.willEnter}
+        willLeave={this.willLeave}
+      >
+        {(interpolated) => (
+          <div>
+            {interpolated.map((item) => (
+              <div style={item.style} key={item.key}>
+                <PersonCard person={person} />
+              </div>
+            ))}
+          </div>
+        )}
+      </TransitionMotion>
     )
+  }
+
+  willEnter = () => ({
+    opacity: 0
+  })
+
+  willLeave = () => ({
+    opacity: spring(0, { stiffness: 15, dumping: 40 })
+  })
+
+  getStyles() {
+    return this.props.people.map((person) => ({
+      key: person.uid,
+      style: {
+        opacity: spring(1, { stiffness: 10, dumping: 30 })
+      },
+      data: person
+    }))
   }
 }
 
