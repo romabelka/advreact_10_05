b4c17086cddd3f6ebb98aa53a1620e072218a422
Roman Iakobchuk <romania@wix.com>
2018-06-07 21:00:18 +0300

    add image

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 //import SignIn from './src/components/sign-in'
 //import HelloWorld from './src/hello-world'
 import EventList from './src/components/event-list'
 import fixtures from './fixtures.json'
+import img from './assets/images/logo.png'
 
 
 const events = Object.entries(fixtures.events).map(([uid, event]) => ({...event, uid}))
@@ -12,6 +13,10 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+          <Image style = {styles.image}
+                 source = {img}
+                 resizeMode = {Image.resizeMode.contain}
+          />
           <EventList events = {events}/>
       </View>
     );
@@ -27,4 +32,9 @@ const styles = StyleSheet.create({
     paddingTop: 30,
     paddingBottom: 30,
   },
+    image: {
+        width: '100%',
+        height: 100
+    }
 });
+
