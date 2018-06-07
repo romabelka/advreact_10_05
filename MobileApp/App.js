886754270b376012d395db613a40fa28034e93e7
Roman Iakobchuk <romania@wix.com>
2018-06-07 20:52:35 +0300

    add ScrollView

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import SignIn from './src/sign-in'
-import HelloWorld from './src/hello-world'
+//import SignIn from './src/components/sign-in'
+//import HelloWorld from './src/hello-world'
+import EventList from './src/components/event-list'
+import fixtures from './fixtures.json'
+
+
+const events = Object.entries(fixtures.events).map(([uid, event]) => ({...event, uid}))
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-          <HelloWorld/>
+          <EventList events = {events}/>
       </View>
     );
   }
@@ -19,5 +24,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 30,
+    paddingBottom: 30,
   },
 });
