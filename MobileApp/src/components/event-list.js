2d0c68b6cbcba3cb8926ca42ea6e986df140b36d
TiesP <pavel.o.71@gmail.com>
2018-06-10 09:51:02 +0300

    SectionList + shadow Android

--- MobileApp/src/components/event-list.js
+++ MobileApp/src/components/event-list.js
@@ -1,26 +1,41 @@
-import React, { Component } from 'react'
-import {ScrollView, StyleSheet, Text} from 'react-native'
-import Card from './common/card'
+import React, {Component} from 'react'
+import {Text, SectionList} from 'react-native'
 
 class EventList extends Component {
-    static propTypes = {
+  static propTypes = {};
 
-    };
-
-    render() {
-        return (
-            <ScrollView>
-                {this.props.events.map(event =>
-                    <Card key = {event.uid}>
-                        <Text>{event.title}</Text>
-                    </Card>
-                )}
-            </ScrollView>
-        )
+  render() {
+    const reducer = (acc, event) => {
+      const letter = event.title.charAt(0).toUpperCase()
+      if (!acc[letter]) {
+        acc[letter] = [event]
+      } else {
+        acc[letter].push(event)
+      }
+      return acc
     }
-}
+    const secObj = this.props.events.reduce(reducer, {})
 
-const styles = StyleSheet.create({
-})
+    const secArrObj = Object.keys(secObj).reduce(
+      (acc, key) => {
+        acc.push({title: key, data: secObj[key]})
+        return acc
+      },
+      []
+    )
+
+    return (
+
+      <SectionList
+        renderItem={({item}) => <Text key={item.uid}>{item.title}</Text>}
+        renderSectionHeader={({section: {title}}) => (
+          <Text style={{fontWeight: 'bold'}}>{title}</Text>
+        )}
+        sections={secArrObj}
+        keyExtractor={(item, index) => item + index}
+      />
+    )
+  }
+}
 
 export default EventList
