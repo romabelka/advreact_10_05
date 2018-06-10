858039f4351d0d3187169f1eeac3ec77836af7f7
Timur <timurgrenda@gmail.com>
2018-06-10 22:44:39 +0300

    sorted SectionList

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -9,6 +9,34 @@ import img from './assets/images/logo.png'
 
 const events = Object.entries(fixtures.events).map(([uid, event]) => ({...event, uid}))
 
+const groupedEvents = Object.entries(fixtures.events).reduce( (acc, [uid, event]) => {
+  console.log(event);
+  const firstChar = event.title[0].toLowerCase();
+  if(firstChar in acc) {
+    acc[firstChar].push({...event, uid})
+  } else {
+    acc[firstChar] = [{...event, uid}]
+  }
+
+  return acc;
+},{});
+
+const sections = Object.entries(groupedEvents).map( ([firstChar, eventsArray]) => {
+  return {
+    title: firstChar,
+    data: eventsArray,
+  }
+});
+
+const sortingFn = (a, b) => {
+  if(a.title < b.title) return -1
+  if(a.title > b.title) return 1
+  return 0;
+}
+
+const sortedSections = sections.sort(sortingFn)
+
+
 export default class App extends React.Component {
   render() {
     return (
@@ -17,7 +45,7 @@ export default class App extends React.Component {
                  source = {img}
                  resizeMode = {Image.resizeMode.contain}
           />
-          <EventList events = {events}/>
+          <EventList events = {events} sections={sortedSections}/>
       </View>
     );
   }
