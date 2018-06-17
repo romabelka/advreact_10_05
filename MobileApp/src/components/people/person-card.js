e9a0863056556f525a99257b859db5c014c5a3f4
omiceron <omiceron@gmail.com>
2018-06-17 18:52:56 +0500

    HT8: Avatar implemented

--- MobileApp/src/components/people/person-card.js
+++ MobileApp/src/components/people/person-card.js
@@ -1,21 +1,36 @@
 import React, {Component} from 'react'
-import {View, Text, Image, StyleSheet} from 'react-native'
+import {View, Text, Image, StyleSheet, ActivityIndicator} from 'react-native'
 import Card from '../common/card'
 import firebase from 'firebase/app'
+import {config} from '../../config'
+import {observable, action} from 'mobx'
+import {observer} from 'mobx-react'
 
+@observer
 class PersonCard extends Component {
   static propTypes = {};
 
+  @observable loading = false
+  @observable loaded = false
+  @observable avatarURL = null
 
-  componentDidMount() {
-    this.getAvatar(this.props.person)
+  async componentDidMount() {
+    this.setLoading(true)
+    await this.getAvatar(this.props.person)
   }
 
+  @action setAvatar = (url) => this.avatarURL = url
+  @action setLoading = loading => this.loading = loading
+  @action setLoaded = loaded => this.loaded = loaded
+
   render() {
-    const {email, firstName, lastName, avatar, uid} = this.props.person
+    const {email, firstName, lastName} = this.props.person
+    const avatar = <Image source={{uri: this.avatarURL}} style={styles.avatar}/>
+    const loader = <View style={styles.avatar}><ActivityIndicator size="large"/></View>
+
     return (
       <Card style={styles.container}>
-        <Image source={{uri: 'https://picsum.photos/200/100/?random'}} style={styles.avatar}/>
+          {this.loading || !this.loaded || !this.avatarURL ? loader : avatar}
         <View style={styles.content}>
           <Text style={styles.email}>{email}</Text>
           <Text>{firstName} {lastName}</Text>
@@ -24,34 +39,46 @@ class PersonCard extends Component {
     )
   }
 
-  getAvatar = ({uid, avatar}) => {
-    if (!avatar) return
+  getAvatar = async ({uid, avatar}) => {
+    if (!avatar) {
+      await this.setAvatar('https://picsum.photos/200/100/?random')
+    } else {
+      await firebase.storage()
+        .refFromURL(`gs://${config.storageBucket}/avatars/${uid}/${avatar}`)
+        .getDownloadURL()
+        .then(url => this.setAvatar(url))
+    }
 
-    firebase.storage()
-      .refFromURL(`gs://advancedreactcourse.appspot.com/avatars/${uid}/${avatar}`)
-      .getDownloadURL()
-      .then(console.log);
+    this.setLoading(false)
+    this.setLoaded(true)
 
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row'
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-around',
   },
   avatar: {
     width: 200,
     height: 100,
-    margin: 5
+    margin: 5,
+    justifyContent: 'center',
+
   },
   content: {
     flexDirection: 'column',
     justifyContent: 'space-around',
-    alignItems: 'center'
+    alignItems: 'center',
+
   },
   email: {
     fontWeight: 'bold'
   }
 })
+
 
 export default PersonCard
